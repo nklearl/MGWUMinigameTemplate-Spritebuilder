5817dc0eb6466dda5cc6d14bd941974285c77cfe
K <k@Ks-iMac.local>
2014-07-27 11:58:20 -0400

    control added

---
+++ MGWUMinigameTemplate.spritebuilder/MyCoin.h
@@ -0,0 +1,13 @@
+//
+//  coin.h
+//  MGWUMinigameTemplate
+//
+//  Created by K on 7/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface coin : CCSprite
+
+@end
---
+++ MGWUMinigameTemplate.spritebuilder/MyCoin.m
@@ -0,0 +1,13 @@
+//
+//  coin.m
+//  MGWUMinigameTemplate
+//
+//  Created by K on 7/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "coin.h"
+
+@implementation coin
+
+@end
